940f17dd583e2f0e0d2e343f21cd4c49feb1fc15
Tom Leman <tomleman@gmail.com>
2013-07-05 10:29:53 +0100

    Initial Commit. Files and folder structure created

---
+++ application.rb
@@ -0,0 +1,37 @@
+require 'sinatra/base'
+require 'data_mapper'
+
+# require_relative './lib/user'
+# require_relative './ddby_helpers'
+# require_relative './dm-setup'
+
+class Ddby <Sinatra::Base
+
+	# include DdbyHelpers
+
+	get '/' do
+		'Hello, World!'
+	end
+
+	get '/login' do
+
+	end
+
+	get '/signup' do
+
+	end
+
+	post '/login' do
+
+	end
+
+	post '/signup' do
+
+	end
+
+	get '/logout' do
+		redirect to ('/')
+	end	
+
+end
+
